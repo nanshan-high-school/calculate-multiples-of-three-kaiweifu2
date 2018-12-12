6cfb771d8964a6a68b01c59d9f8e0fcfe0baf97e
KaKakaKakkkKAkkakkAkkkAkAAkakkakakakakAkAkAkAkkA <45233906+kaiweifu2@users.noreply.github.com>
2018-12-12 23:56:57 +0800

    Create 未完成.cpp

---
+++ 未完成.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+int num1, num2, num3;
+cout<<"請輸入數字1:";
+cin>>num1;
+cout<<"請輸入數字2:";
+cin>>num2;
+num3= num1 * num2;
+cout<<num3;
+
+}
